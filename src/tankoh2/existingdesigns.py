606ae3febd13bc854a40e61daba2cd2c162acade
Sebastian Freund <sebastian.freund@dlr.de>
2021-10-29 12:39:15 +0200

    module for existing designs to be used in control_sf.createWindingDesign

---
+++ src/tankoh2/existingdesigns.py
@@ -0,0 +1,17 @@
+""""""
+
+
+# HyMod
+# 12mm thickness in cylindrical section
+#
+
+hymodDesign = dict([('burstPressure', 77.85),
+                    ('lzyl', 1000.),
+                    ('minPolarOpening', 23),
+                    ('dzyl', 300.)
+                    ])
+
+
+
+
+
