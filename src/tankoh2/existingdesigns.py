5b6efeafde2f8b5f52104ca055fa41759c0bcccc
Sebastian Freund <sebastian.freund@dlr.de>
2021-11-11 11:29:53 +0100

    merge both last commits

--- src/tankoh2/existingdesigns.py
+++ src/tankoh2/existingdesigns.py
@@ -51,25 +51,25 @@ hymodDesign = OrderedDict([('burstPressure', 77.85),
 
 
 NGTBITDesign = OrderedDict([('tankname', 'NGT-BIT-2020-09-16'),
-                     ('pressure', 70),                                        
-                    # Geometry
-                    ('minPolarOpening', 23),
-                    ('dzyl', 400.),
-                    ('lzyl', 500.),                    
-                    # design philosophy
-                    ('safetyFactor', 2.0),
-                    ('useFibreFailure', True), 
-                    # material 
-                    ('materialname', 'CFRP_T700SC_LY556'),
-                    # fibre roving parameter
-                    ('hoopLayerThickness', 0.125),
-                    ('helixLayerThickenss', 0.129),
-                    ('rovingWidth', 3.175),
-                    ('numberOfRovings', 12),
-                    ('tex', 800),
-                    ('fibreDensity', 1.78),
-                    # optimizer settings
-                    ('maxlayers', 200)                                                            
-                    ])
+                            ('pressure', 70),
+                            # Geometry
+                            ('minPolarOpening', 23),
+                            ('dzyl', 400.),
+                            ('lzyl', 500.),
+                            # design philosophy
+                            ('safetyFactor', 2.0),
+                            ('useFibreFailure', True),
+                            # material
+                            ('materialname', 'CFRP_T700SC_LY556'),
+                            # fibre roving parameter
+                            ('hoopLayerThickness', 0.125),
+                            ('helixLayerThickenss', 0.129),
+                            ('rovingWidth', 3.175),
+                            ('numberOfRovings', 12),
+                            ('tex', 800),
+                            ('fibreDensity', 1.78),
+                            # optimizer settings
+                            ('maxlayers', 200)
+                            ])
 
 
