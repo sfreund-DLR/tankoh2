806028bf3210a759a3d38869ee79b53e4268662f
Sebastian Freund <sebastian.freund@dlr.de>
2022-03-24 08:48:23 +0100

    adjust test

--- test/test_design/test_designuitls.py
+++ test/test_design/test_designuitls.py
@@ -7,13 +7,13 @@ def test_getRequiredVolume():
     #from brewer ch 4.4.1
     m = 30815  # lb
     p = 21  # psi
-    v = getRequiredVolume(m, p, roh=4.326)
+    v = getRequiredVolume(m, p, roh=4.326, maxFill=1/1.072)
     vRef = 7636 # lb/ft3
     assert abs(1-v/vRef) < 2e-5
 
 def test_getLengthRadiusFromVolume():
     r1,l1 = getLengthRadiusFromVolume(100 * 1e6, # l → mm**3
-                                      mode='quick')
-    r2,l2 = getLengthRadiusFromVolume(100 * 1e6)
+                                      mode='quick', domeType='ellipse')
+    r2,l2 = getLengthRadiusFromVolume(100 * 1e6, domeType='ellipse')
     assert np.allclose([r1,l1], [r2,l2])
 
