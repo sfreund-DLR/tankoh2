8477a254c357335a30728243b2ef0abd379a1360
Sebastian Freund <sebastian.freund@dlr.de>
2022-06-21 15:34:55 +0200

    #42 introduce axis instance based plotting of vlines to not confuse the axes

--- src/tankoh2/service/plot/generic.py
+++ src/tankoh2/service/plot/generic.py
@@ -1,49 +1,48 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pylab as plt
+from itertools import zip_longest
 
 
-def plotDataFrame(show, filename, dataframe, axes=None, vlines=None, vlineColors=None, title=None,
+def plotDataFrame(show, filename, dataframe, ax=None, vlines=None, vlineColors=[], title=None,
                   yLabel=None, xLabel=None, plotKwArgs = None):
     """plots puck properties
 
     :param show: show the plot created
     :param filename: save the plot to filename
     :param dataframe: dataframe with layers as columns and elementIds as index
-    :param axes: matplotlib axes object
+    :param ax: matplotlib axes object
     :param vlines: x-coordinates with a vertical line to draw
     """
-    if axes is None:
+    if ax is None:
         fig = plt.figure(figsize=(15,9))
-        ax = fig.gca()
+        useAx = fig.gca()
     else:
-        ax = axes
+        useAx = ax
     if plotKwArgs is None:
         plotKwArgs = {}
     useLegend = plotKwArgs.get('legend', True)
     if 'legend' in plotKwArgs:
         plotKwArgs.pop('legend')
-    dataframe.plot(ax=ax, legend=False, **plotKwArgs)
+    dataframe.plot(ax=useAx, legend=False, **plotKwArgs)
     if useLegend:
-        legendKwargs = {'bbox_to_anchor':(1.05, 1), 'loc':'upper left'} if axes is None else {'loc':'best'}
-        ax.legend(**legendKwargs)
-    ax.set(xlabel='' if xLabel is None else xLabel,
+        legendKwargs = {'bbox_to_anchor':(1.05, 1), 'loc':'upper left'} if ax is None else {'loc': 'best'}
+        useAx.legend(**legendKwargs)
+    useAx.set(xlabel='' if xLabel is None else xLabel,
            ylabel='' if yLabel is None else yLabel,
            title='' if title is None else title)
 
     if vlines is not None:
-        if vlineColors is None:
-            vlineColors = 'black'
         ymin, ymax = dataframe.min().min(), dataframe.max().max()
-        plt.vlines(vlines, ymin, ymax + 0.1*(ymax-ymin), colors=vlineColors, linestyles='dashed')
+        ymax += 0.1*(ymax-ymin)
 
-    if axes is None:
+        for vline, color in zip_longest(vlines, vlineColors, fillvalue='black'):
+            useAx.plot([vline, vline], (ymin, ymax), color=color, linestyle='dashed')
+        #plt.vlines(vlines, ymin, ymax + , colors=vlineColors, linestyles='dashed')
+
+    if ax is None:
         plt.subplots_adjust(right=0.75, left=0.10)
-        if filename:
-            plt.savefig(filename)
-        if show:
-            plt.show()
-        plt.close(fig)
+        saveShowClose(filename, show, fig)
 
 
 def plotContour(show, filename, x, r, title, parameter, ax = None, plotContourCoordinates = True, **mplKwargs):
@@ -67,20 +66,30 @@ def plotContour(show, filename, x, r, title, parameter, ax = None, plotContourCo
         fig, axs = plt.subplots(1, plotCount, figsize=(17, 5))
     if plotContourCoordinates:
         df = pd.DataFrame(np.array([x,r]).T, columns=['x','r'])
-        plotDataFrame(show, None, df, axes=axs[0], title=title, yLabel='x,r', xLabel='Contour Coordinate',
+        plotDataFrame(show, None, df, ax=axs[0], title=title, yLabel='x,r', xLabel='Contour index',
                       plotKwArgs= mplKwargs)
     useAx = axs[-1] if ax is None else ax
     df = pd.DataFrame(np.array([r]).T, columns=[parameter], index=pd.Index(x))
     mplKwargs = mplKwargs.copy()
     mplKwargs.update([('legend', False)])
-    plotDataFrame(show, None, df, axes=useAx, title=title, yLabel='r', xLabel='x', plotKwArgs= mplKwargs)
+    plotDataFrame(show, None, df, ax=useAx, title=title, yLabel='r', xLabel='x', plotKwArgs= mplKwargs)
     useAx.set_aspect('equal', adjustable='box')
 
     plt.axis('scaled')
 
+    if ax is None:
+        saveShowClose(filename, show, fig)
+
+
+def saveShowClose(filename = '', show=False, fig=None):
+    """saves and shows the actual plot and closes it afterwards
+    :param filename: filename to save the plot. Not saved if not given
+    :param show: shows the plot
+    :param fig: figure object to be closed
+    """
     if filename:
         plt.savefig(filename)
     if show:
         plt.show()
-    if not ax:
+    if fig is not None:
         plt.close(fig)
